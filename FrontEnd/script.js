d4734c4375f6655018b9807ad972c4c04ced5132
RemadiIslem <islem.remadi69@gmail.com>
2025-12-05 11:36:46 +0100

    Add button categories and filters

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,44 @@
-async function getWorks () {
-const url = "http://localhost:5678/api/"
+async function getWorks (filter) {
+document.querySelector(".gallery").innerHTML = "";
+const url = "http://localhost:5678/api/works"
+try {
+    const response = await fetch(url)
+    if(!response.ok){
+        throw new Error (`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    if (filter) {
+      const filtered = json.filter((data) => data.categoryId === filter);
+      for (let i = 0; i < filtered.length; i++) {
+        setFigure(filtered[i]);
+      }
+    } else {
+      for (let i = 0; i < json.length; i++) {
+        setFigure(json[i]);
+      }
+    }
+   
+} catch (error){
+    console.error(error.message);
+}
+
+}
+
+
+function setFigure (data){
+const figure = document.createElement("figure");
+figure.innerHTML = 
+				`<img src= ${data.imageUrl} alt= ${data.title}>
+				<figcaption>${data.title}</figcaption>`
+			
+document.querySelector(".gallery").append(figure);
+}
+getWorks();
+
+
+async function getCategories () {
+const url = "http://localhost:5678/api/categories"
 try {
     const response = await fetch(url)
     if(!response.ok){
@@ -9,7 +48,7 @@ try {
     const json = await response.json();
     console.log(json)
      for(i=0;i < json.length; i++){
-       setFigure(json[i]);
+       setFilter(json[i]);
 
 
     }
@@ -19,13 +58,15 @@ try {
 
 }
 
-getWorks();
 
-function setFigure (data){
-const figure = document.createElement(figure);
-figure.innerHTML = 
-				`<img src= ${data.imageUrl} alt= ${data.title}>
-				<figcaption>${data.title}</figcaption>`
-			
-document.querySelector(".gallery").append(figure);
+function setFilter (data){
+const div = document.createElement("div");
+div.className= data.id;
+div.addEventListener("click",()=> getWorks(data.id));
+div.innerHTML = `${data.name}`;	
+document.querySelector(".div-container").append(div);
 }
+getCategories();
+
+document.querySelector(".tous").addEventListener("click",()=> getWorks());
+
